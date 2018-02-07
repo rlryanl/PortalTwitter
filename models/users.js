5a81513e150b6dea177a4a2b420204232e2b057e
Ryan <RL.RyanLMail@gmail.com>
2018-02-07 11:55:55 -0800

    Minor UI Changes and User/Tweet models

--- models/users.js
+++ models/users.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.schema({
-
+    username: {type: String, unique: true, required: true},
+    handle: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    tweets: {type: [tweet], "default": []}
 });
 
 var model = mongoose.model('users', schema);
